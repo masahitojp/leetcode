401c6329643b9976b4b7bb496f30d7a8030aaae4
masahitojp <randomstep@gmail.com>
2021-01-10 22:01:08 +0900

    3597.word ladder

--- python/main/word_ladder.py
+++ python/main/word_ladder.py
@@ -1,81 +1,130 @@
-from typing import List
+from typing import List, Set
 
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-
-        if endWord in wordList:
-            ans = 1
-            curr = beginWord
-            temp = wordList
-            if beginWord in temp:
-                temp.remove(beginWord)
-
-            #  curr == endWordなら 打ち切り
-
-            while curr != endWord:
-                # 1文字違いの単語を探す
-                #  ^ の単語を削除して、 ans++
-                try:
-                    for i in range(len(temp)):
-                        print(curr, temp)
-                        t = [x for x in temp if self.check(curr, x)]
-                        print(t)
-                        if endWord in t:
-                            curr = endWord
-                            ans += 1
-                            print(curr)
-                            raise Exception
-                        elif len(t) == 0:
-                            return 0
-                        elif curr == t[0]:
-                            print(curr)
-                            temp.remove(curr)
-                            raise Exception
-                        else:
-                            s = [x for x in t if self.check(endWord, x)]
-                            if len(s) == 0:
-                                curr = t[0]
-                                print(curr)
-                            else:
-                                curr = s[0]
-                            temp.remove(curr)
-                            ans += 1
-                            raise Exception
-                except Exception:
-                    pass
-            return ans
-        else:
-            return 0
-
-    def check(self, x: str, y: str) -> bool:
-        def levenshtein_distance(a, b):
-            m = [[0] * (len(b) + 1) for i in range(len(a) + 1)]
-
-            for i in range(len(a) + 1):
-                m[i][0] = i
-
-            for j in range(len(b) + 1):
-                m[0][j] = j
-
-            for i in range(1, len(a) + 1):
-                for j in range(1, len(b) + 1):
-                    if a[i - 1] == b[j - 1]:
-                        x = 0
-                    else:
-                        x = 1
-                    m[i][j] = min(m[i - 1][j] + 1, m[i][j - 1] + 1, m[i - 1][j - 1] + x)
-            # print m
-            return m[-1][-1]
-
-        return levenshtein_distance(x, y) <= 1
+        word_set: Set[str] = set(wordList)
+        count: int = 1
+        prev: List[int] = [beginWord]
+        while len(prev) != 0:
+            count += 1
+            next = []
+            for word in prev:
+                for i in range(0, len(word)):
+                    prefix: str = word[:i]
+                    suffix: str = word[(i + 1) :]
+                    for c in range(ord("a"), ord("z") + 1):
+                        next_word = prefix + chr(c) + suffix
+                        if not (next_word in word_set):
+                            continue
+                        if next_word == endWord:
+                            return count
+                        word_set.remove(next_word)
+                        next.append(next_word)
+            # print(count, next, word_set)
+            prev = next
+        return 0
 
 
 if __name__ == "__main__":
-    print(
-        Solution().ladderLength(
-            "hot",
-            "dog",
-            ["hot", "dog", "dot"],
-        )
+    Solution().ladderLength(
+        "qa",
+        "sq",
+        [
+            "si",
+            "go",
+            "se",
+            "cm",
+            "so",
+            "ph",
+            "mt",
+            "db",
+            "mb",
+            "sb",
+            "kr",
+            "ln",
+            "tm",
+            "le",
+            "av",
+            "sm",
+            "ar",
+            "ci",
+            "ca",
+            "br",
+            "ti",
+            "ba",
+            "to",
+            "ra",
+            "fa",
+            "yo",
+            "ow",
+            "sn",
+            "ya",
+            "cr",
+            "po",
+            "fe",
+            "ho",
+            "ma",
+            "re",
+            "or",
+            "rn",
+            "au",
+            "ur",
+            "rh",
+            "sr",
+            "tc",
+            "lt",
+            "lo",
+            "as",
+            "fr",
+            "nb",
+            "yb",
+            "if",
+            "pb",
+            "ge",
+            "th",
+            "pm",
+            "rb",
+            "sh",
+            "co",
+            "ga",
+            "li",
+            "ha",
+            "hz",
+            "no",
+            "bi",
+            "di",
+            "hi",
+            "qa",
+            "pi",
+            "os",
+            "uh",
+            "wm",
+            "an",
+            "me",
+            "mo",
+            "na",
+            "la",
+            "st",
+            "er",
+            "sc",
+            "ne",
+            "mn",
+            "mi",
+            "am",
+            "ex",
+            "pt",
+            "io",
+            "be",
+            "fm",
+            "ta",
+            "tb",
+            "ni",
+            "mr",
+            "pa",
+            "he",
+            "lr",
+            "sq",
+            "ye",
+        ],
     )
