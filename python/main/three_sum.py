85714c9b3987cd0586bb0c048801007e4710380a
masahitojp <randomstep@gmail.com>
2021-01-16 23:36:39 +0900

    15. 3Sum: solved but too late(5088 ms)

--- python/main/three_sum.py
+++ python/main/three_sum.py
@@ -3,16 +3,33 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        len_nums = len(nums)
-        ans = []
-        if len_nums < 3:
-            return []
-        for i in range(len_nums - 2):
-            for j in range(i + 1, len_nums - 1):
-                for k in range(j + 1, len_nums):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        temp = [nums[i], nums[j], nums[k]]
-                        if not (temp in ans):
-                            ans.append(temp)
-        print(ans)
+        n: int = len(nums)
+        ans: List[List[int]] = []
+        if n == 0:
+            return ans
+        nums.sort()
+        for i in range(n):
+            lp: int = i + 1
+            rp: int = n - 1
+            while lp < rp:
+                total_sum: int = nums[i] + nums[lp] + nums[rp]
+                if total_sum == 0:
+                    temp: List[int] = [nums[i], nums[lp], nums[rp]]
+                    if not (temp in ans):
+                        ans.append(temp)
+                    l_num: int = nums[lp]
+                    while lp < n - 1 and l_num == nums[lp]:
+                        lp += 1
+
+                    r_num: int = nums[rp]
+                    while lp < rp and nums[rp] == r_num:
+                        rp -= 1
+                elif total_sum > 0:
+                    rp -= 1
+                else:
+                    lp += 1
+
+            while i < n - 1 and nums[i] == nums[i + 1]:
+                i += 1
+
         return ans
