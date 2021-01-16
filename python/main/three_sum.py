e11fa12780403eacb8584e7165abd211f4924b75
masahitojp <randomstep@gmail.com>
2021-01-16 23:53:11 +0900

    15. 3Sum: solved but too 5088 ms -> 2912 ms

--- python/main/three_sum.py
+++ python/main/three_sum.py
@@ -1,22 +1,23 @@
-from typing import List
+from typing import List, Set
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n: int = len(nums)
         ans: List[List[int]] = []
-        if n == 0:
+        if n < 3:
             return ans
         nums.sort()
+        pair: Set[int] = set()
         for i in range(n):
             lp: int = i + 1
             rp: int = n - 1
             while lp < rp:
                 total_sum: int = nums[i] + nums[lp] + nums[rp]
                 if total_sum == 0:
-                    temp: List[int] = [nums[i], nums[lp], nums[rp]]
-                    if not (temp in ans):
-                        ans.append(temp)
+                    if not ((nums[i], nums[lp]) in pair):
+                        ans.append([nums[i], nums[lp], nums[rp]])
+                        pair.add((nums[i], nums[lp]))
                     l_num: int = nums[lp]
                     while lp < n - 1 and l_num == nums[lp]:
                         lp += 1
